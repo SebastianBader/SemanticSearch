2f50131242b01163c03f3ea5a67904d2b6207863
Sebastian Bader <98041394+vac-mmis@users.noreply.github.com>
2024-09-05 18:30:25 +0200

    gui

--- semantic_search.py
+++ semantic_search.py
@@ -1,18 +1,20 @@
 import streamlit as st
 
+# on streamlit - there is an old version of sqlite3!!!
+# the following lines replace it with an up-to-date version
 import pysqlite3
 import sys
 sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 
+# Bow we can import the tweaked version
 import sqlite3
-
 
 import chromadb
 
 client = chromadb.PersistentClient(path="chromaDB")
 
+# create a side bar with system and db-infos
 st.set_page_config(initial_sidebar_state="collapsed")
-
 st.sidebar.markdown("# Collection")
 # collection_name = st.sidebar.selectbox('Choose Database', [c.name for c in client.list_collections()], 0, label_visibility='collapsed')
 
@@ -25,34 +27,26 @@ collection_info += f"- Name: '{collection_name}'\n"
 collection_info += f"- Embedding dimension: {collection._model.dimension}\n"
 collection_info += f"- Number of documents: {collection.count()} documents\n"
 
-
 if 'description' in collection.metadata:
     collection_info += f"### Description\n"
     collection_info += "_"+collection.metadata['description'] +"_\n"
 
-
 collection_info += f"### System\n"
 collection_info += f"- SQLite: '{sqlite3.sqlite_version}'\n"
 
-
 st.sidebar.markdown(collection_info)
-  
-# f"""
-# # Information:
-# - Distance norm: '{collection._model.configuration_json['hnsw_configuration']['space']}'
-# """)
+
+
 n_results = 5
 
-
-st.header(f"_:blue[Fully Local Semantic Search]_")
+st.header(f"_:blue[Semantic Search based on Vector Embeddings]_")
 st.markdown(
     f"The following search query, will be embedded into a {collection._model.dimension}-dimensional vector."
-    f" Afterwards, this embedding is used to identify the {n_results} closest documents according to the '{collection.get_model().metadata.get('hnsw:space')}' similarity."
+    f" Afterwards, this embedding is used to identify the {n_results} closest documents withn the collection _'{collection_name}'_ according to the '{collection.get_model().metadata.get('hnsw:space')}' similarity."
     f" The collection _'{collection_name}'_ contains {collection.count()} documents."
 )
 
 question = st.text_input("Search question:", "Who is omnipotent?")
-
 
 results = collection.query(query_texts=[question], n_results=n_results, include=['metadatas', 'documents', 'distances'])
 
